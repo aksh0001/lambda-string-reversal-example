25f33a1ad81382a0d27a556fcd9fcdb9a52616d8
akshay <akshay.k1738@gmail.com>
2020-07-20 21:03:29 -0500

    Added pom aws dependency and Lambda handler

---
+++ src/main/java/StringReversalLambdaHandler.java
@@ -0,0 +1,39 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Map;
+
+public class StringReversalLambdaHandler implements RequestHandler<Map<String, String>, String> {
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public String handleRequest(Map<String, String> event, Context context) {
+        LambdaLogger logger = context.getLogger();
+        String response = new String("200 OK");
+        // log execution details
+        logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
+        logger.log("CONTEXT: " + gson.toJson(context));
+        // process event
+        logger.log("EVENT: " + gson.toJson(event));
+        logger.log("EVENT TYPE: " + event.getClass().toString());
+
+        String reversedStr1 = reverseString(event.get("message1"));
+        String reversedStr2 = reverseString(event.get("msg2"));
+
+        response = String.format("{message1: %s, message2: %s}", reversedStr1, reversedStr2);
+
+        return response;
+    }
+
+    /**
+     * Reverses a string s and returns the result.
+     *
+     * @param s string to be reversed
+     * @return reversed string
+     */
+    private static String reverseString(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+}
